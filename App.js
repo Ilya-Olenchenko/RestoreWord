8f225a5f88b8da7fb128112405fd31c7305baa35
Ілля <olenchenko.ilya@ukr.net>
2022-11-25 14:04:05 +0200

    load+

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
 import React, { useState } from 'react'
 import Stack from './pages/navigation'
 import * as Font from 'expo-font'
+import Apploading from "expo-app-loading";
 
-export default function App() {
+const getFonts = () =>
   Font.loadAsync({
     'robot-media': require('./assets/fonts/Roboto-Medium.ttf'),
+    'robot-regular': require('./assets/fonts/Roboto-Regular.ttf'),
     'alkalami-regula': require('./assets/fonts/Alkalami-Regular.ttf'),
     'merriweather-regular': require('./assets/fonts/Merriweather-Regular.ttf')
+  });
 
-  })
+export default function App() {
+  const [fontsloaded, setFontsLoaded] = useState(false);
 
-  return (
-    <Stack />
-  )
+  if (fontsloaded) {
+    return (
+      <Stack />
+    );
+  } else {
+    return (
+      <Apploading
+        startAsync={getFonts}
+        onFinish={() => {
+          setFontsLoaded(true);
+        }}
+        onError={console.warn}
+      />
+    );
+  }
 }
